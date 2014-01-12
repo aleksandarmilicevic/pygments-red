8eb69cfc61237a2a2f3c1b62cda6515530c20d8f
Aleksandar Milicevic <aleks@csail.mit.edu>
2014-01-12 08:01:23 -0500

    added keywords for mult_mult

--- pygments_red/__init__.py
+++ pygments_red/__init__.py
@@ -119,7 +119,8 @@ class ARbyLexer(Ruby193Lexer):
 
     CLASS_GEN_KEYWORDS = ['sig', 'abstract', 'alloy_model', 'alloy_module', 'alloy']
     OPS_KEYWORDS = ['extends', 'set', 'seq', 'one', 'lone', 'no', 'all', 'some', 'exist']
-    FUN_KEYWORDS = ['fun', 'pred', 'assertion', 'fact', 'check', 'run', 'this', 'not_in?', 'in?', 'ordered', 'enum', 'iden', 'univ', 'let']
+    FUN_KEYWORDS = ['fun', 'pred', 'assertion', 'fact', 'check', 'run', 'this', 'not_in?', 'in?',
+                    'ordered', 'enum', 'iden', 'univ', 'let', 'one_one', 'one_lone', 'lone_one', 'lone_one']
 
     EXTRA_KEYWORDS = CLASS_GEN_KEYWORDS + OPS_KEYWORDS + FUN_KEYWORDS
 
